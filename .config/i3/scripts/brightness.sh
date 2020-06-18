afe3d764b22ae977b5523bbe40e859f200084dcd
sriram98v <vijendran.sriram@gmail.com>
2020-06-19 00:14:37 +0530

    Added brightness.sh

---
+++ .config/i3/scripts/brightness.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+let a=$(cat /sys/class/backlight/intel_backlight/brightness)
+
+if [[ $1 == "-i" ]]; then
+	num=$(($a + $2))
+elif [[ $1=="-d" ]]; then
+	num=$(($a - $2))
+else
+	echo "Invalid Arg"
+fi
+
+if [[ $num -gt 120000 ]]; then
+	num=120000
+elif [[ $num -lt 100 ]]; then
+	num=100
+fi
+
+echo $num > /sys/class/backlight/intel_backlight/brightness
