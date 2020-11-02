6db6d68b233229acf7be361191da910e9afdda1d
sriram98v <vijendran.sriram@gmail.com>
2020-11-02 18:09:37 +0530

    switched picom to ibhagwan, changed rofi

--- .config/i3/scripts/launch_dualmon.sh
+++ .config/i3/scripts/launch_dualmon.sh
@@ -7,3 +7,5 @@ if [[ $externmon = *connected* ]]; then
     xrandr --output HDMI-1-0 --auto --right-of eDP-1 --mode 1920x1080
     feh --bg-scale /home/kuuhaku/Pictures/wallpaper.png /home/kuuhaku/Pictures/wallpaper.png
 fi
+
+pacmd set-default-sink 1
