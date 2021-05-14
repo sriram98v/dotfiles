bf7cdcbc5df37101bcc54de25e76b0d801c0c969
sriram98v <vijendran.sriram@gmail.com>
2021-05-14 10:33:00 +0530

    updated picom

---
+++ .config/i3/scripts/fm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+i3-scratchpad  -d900x500 -p0,40 -wt -mt -atc thunar
--- .config/i3/scripts/whatsapp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-i3-scratchpad  -d900x600 -p0,40 -wt -mt -atc -- google-chrome-stable --app=https://web.whatsapp.com/
